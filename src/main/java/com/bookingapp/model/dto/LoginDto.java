6b265c5855b11d7bffe01c4ca66e5d90db7ec425
ivanivanovmadzharov <ivanivanovmadzharov@gmail.com>
2026-06-18 12:35:20 +0300

    feat: Add DTOs

---
+++ src/main/java/com/bookingapp/model/dto/LoginDto.java
@@ -0,0 +1,18 @@
+package com.bookingapp.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginDto {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
