410904c030788e099b7eab6469434031e1e1a656
ivanivanovmadzharov <ivanivanovmadzharov@gmail.com>
2026-06-20 10:19:11 +0300

    feat: Add tests

---
+++ src/test/java/com/bookingapp/BookingAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifies the Spring application context starts up successfully
+        // with all beans (security, JPA, MVC) wired correctly.
+    }
+}
